cf8024a156cac494e188a1ec02ebd34cfe419835
Martin <martinroldan889@gmail.com>
2021-04-07 10:58:48 -0300

    [add] Standard para formato y reglas del código

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const App = () => {
-    return (
-        <h1>Hola Mundo</h1>
-    )
+  return (
+    <h1>Hola Mundo</h1>
+  )
 }
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
-    },
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: "html-loader",
-            },
-          ],
-        },
-        {
-            test: /\.css$/,
-            use:[
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                },
-                'css-loader'
-            ]
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-        filename: "./index.html",
-      }),
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    }),
 
-      new MiniCssExtractPlugin({
-          filename: 'assests/[name].css',
-      })
-    ],
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      compress: true,
-      port: 3005,
-      open: true
-    },
-  };
+    new MiniCssExtractPlugin({
+      filename: 'assests/[name].css'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3005,
+    open: true
+  }
+}
