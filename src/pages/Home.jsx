858c031bcace896ea5cada9b4654ce3c315a9ef7
Martin <martinroldan889@gmail.com>
2021-04-07 19:52:53 -0300

    Componente Home creado con sus estilos. Pequeño cambio en webpack.config para el manejo de rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { FiGithub, FiTwitter, FiLinkedin, FiInstagram } from 'react-icons/fi'
+import '../assets/styles/components/Home.css'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="Home">
+      <div className="Home__intro">
+        <h1><span>Hola, <br /> soy Martín Roldán, </span> <br /> <span>Frontend Developer</span></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias unde quod perferendis provident id dolore temporibus impedit.</p>
+      </div>
+      <div className="Home__social">
+        <a href="#" target="_blank">
+          <FiTwitter size="28px" />
+        </a>
+        <a href="#" target="_blank">
+          <FiInstagram size="28px" />
+        </a>
+        <a href="#" target="_blank">
+          <FiLinkedin size="28px" />
+        </a>
+        <a href="#" target="_blank">
+          <FiGithub size="28px" />
+        </a>
+      </div>
+      <div className="Home__button">
+        <a href="/contacto">Contactame</a>
+      </div>
+    </section>
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,7 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3005,
-    open: true
+    open: true,
+    historyApiFallback: true
   }
 }
