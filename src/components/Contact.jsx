8090ca44e5680475a660b125c52ce2ef67a6de7e
Martin <martinroldan889@gmail.com>
2021-04-17 19:33:54 -0300

    Componente Contact creado con sus estilos

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import '../assets/styles/components/Contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <section className="Contact" id="contact">
+      <div className="Contact__info">
+        <h2>Contactame</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vitae placeat consectetur voluptatum fuga dolorum quam minus temporibus, eligendi inventore optio rerum! Quam inventore, eius numquam dolorem error deserunt minima?</p>
+      </div>
+      <div className="Contact__me">
+        <form action="#">
+          <label htmlFor="name">Nombre</label>
+          <input type="text" name="name" id="" placeholder="Nombre"/>
+          <label htmlFor="email">Correo</label>
+          <input type="email" name="email" id="" placeholder="Correo electrónico"/>
+          <label htmlFor="message">Mensaje</label>
+          <textarea name="message" id="" placeholder="Mensaje"/>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
