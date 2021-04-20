cba5511d96f22b71152a71837e83a96d47b2a871
Martin <martinroldan889@gmail.com>
2021-04-19 22:10:54 -0300

    Enviar emails desde el formulario con emailJs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com'
 import '../assets/styles/components/Contact.css'
 
 const Contact = () => {
+  const [ value, setValue ] = useState(null)
+
+  const onHandleSendEmail = e => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_mxrold', 'template_1cyjmfz', e.target, 'user_dRUN4n4ny7idTPbrdVRvs')
+      .then((result) => {
+          setValue(true)
+          console.log(result.text)
+      }, (error) => {
+          setValue(false)
+          console.log(error.text)
+      });
+      e.target.reset()
+  }
+
   return (
     <section className="Contact">
       <div className="Contact__info">
@@ -9,15 +26,26 @@ const Contact = () => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vitae placeat consectetur voluptatum fuga dolorum quam minus temporibus, eligendi inventore optio rerum! Quam inventore, eius numquam dolorem error deserunt minima?</p>
       </div>
       <div className="Contact__me">
-        <form action="#">
+        <form onSubmit={onHandleSendEmail}>
           <label htmlFor="name">Nombre</label>
-          <input type="text" name="name" id="name" placeholder="Nombre"/>
+          <input type="text" name="name" id="name" placeholder="Nombre" required/>
           <label htmlFor="email">Correo</label>
-          <input type="email" name="email" id="email" placeholder="Correo electrónico"/>
+          <input type="email" name="email" id="email" placeholder="Correo electrónico" required/>
           <label htmlFor="message">Mensaje</label>
-          <textarea name="message" id="message" placeholder="Mensaje"/>
+          <textarea name="message" id="message" placeholder="Mensaje" required/>
           <button type="submit">Enviar</button>
+          {/* {
+            value === true &&  
+          }
+          {
+          
+            value === false && 
+          } */}
         </form>
+          
+           
+          
+          
       </div>
     </section>
   )
