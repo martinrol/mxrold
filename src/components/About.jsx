6f772488032116b04ac3d9c5744345e6e746f608
Martin <martinroldan889@gmail.com>
2021-04-20 20:37:40 -0300

    Certificados agregados en acerca de mi y cambios en los estilos. Actualización de la api

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import { AiFillHtml5 } from 'react-icons/ai'
 import { DiCss3 } from 'react-icons/di'
 import { FaReact } from 'react-icons/fa'
 import { SiJavascript } from 'react-icons/si'
+import { courses } from '../../api/works.json'
 import '../assets/styles/components/About.css'
 
 const About = () => {
@@ -12,6 +13,26 @@ const About = () => {
         <h2>Acerca de mí</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis odit dolorem suscipit animi reiciendis mollitia ullam, quam minima totam minus porro iusto officiis architecto. Animi expedita deleniti sed dolorum assumenda.</p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut dicta, repellendus sapiente labore repellat adipisci consectetur laborum ullam officiis eveniet, debitis ad, similique facilis perspiciatis nostrum. Beatae voluptas at velit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error mollitia accusamus sapiente, non aliquam minima? Facilis quam animi quaerat! Dolor esse repellendus sunt quibusdam consectetur laboriosam earum officiis cupiditate laudantium.</p>
+        <div className="About__me--title-courses">
+          <h3>Certificados</h3>
+          <a href="https://platzi.com/p/Martinrol/" target="_blank">
+            Ver mi perfil en Platzi
+          </a>
+        </div>
+        <article className="About__me--courses">
+          {
+            courses.map((item) => (
+              <div className="About__me--courses-item" key={item.id}>
+                <a href={item.url} target="_blank" title={item.title}>
+                  <div>
+                    <p>{item.id}</p>
+                  </div>
+                  <img src={item.image} alt={item.title}/>
+                </a>
+              </div>
+            ))
+          }
+        </article>
       </div>
       <div className="About__skills">
         <div className="About__skills--animation">
