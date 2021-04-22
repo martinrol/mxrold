b1d3b1445e464fbfaf60b3ba5c9a247c667b2978
Martin <martinroldan889@gmail.com>
2021-04-21 21:01:44 -0300

    Información agregada en Acerca de mi. Algunos cambios en los estilos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { AiFillHtml5 } from 'react-icons/ai'
-import { DiCss3, DiGit } from 'react-icons/di'
+import { DiCss3 } from 'react-icons/di'
 import { FaReact } from 'react-icons/fa'
-import { SiJavascript, SiTailwindcss, SiBootstrap, SiStyledComponents, SiBoost } from 'react-icons/si'
+import { SiJavascript, SiTailwindcss, SiBootstrap, SiStyledComponents } from 'react-icons/si'
 import { courses } from '../../api/api.json'
 import '../assets/styles/components/About.css'
 
@@ -10,10 +10,13 @@ const About = () => {
   return (
     <section className="About">
       <div className="About__me">
-        <div class="About__me--info">
+        <div className="About__me--info">
           <h2>Acerca de mí</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis odit dolorem suscipit animi reiciendis mollitia ullam, quam minima totam minus porro iusto officiis architecto. Animi expedita deleniti sed dolorum assumenda.</p>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut dicta, repellendus sapiente labore repellat adipisci consectetur laborum ullam officiis eveniet, debitis ad, similique facilis perspiciatis nostrum. Beatae voluptas at velit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error mollitia accusamus sapiente, non aliquam minima? Facilis quam animi quaerat! Dolor esse repellendus sunt quibusdam consectetur laboriosam earum officiis cupiditate laudantium.</p>
+          <p>¡Hola! Soy desarrollador frontend junior en las tecnologías de HTML, CSS, JavaScript y React.</p>
+          <p>Desde hace un año decidí dedicarme al mundo de la programación y tecnología luego de haber conocido todos los productos digitales y aplicaciones geniales que se pueden crear tan solo con una computadora, internet, creatividad, compromiso y muchas ganas.</p>
+          <p>Soy autodidacta, estudió en la plataforma de educación online Platzi, donde me centro principalmente en el área de desarrollo web y diversos temas de mi interés tales como otros lenguajes de programación, diseño e inglés.</p>
+          <p>Me gusta aprender sobre nuevos contenidos, proponerme retos y mejorar para superarme día a día.</p>
+          <p>Si quieres saber más sobre mi, te invito a que veas algunos de mis proyectos.</p>
         </div>
         <div>
           <div className="About__me--title-courses">
@@ -91,13 +94,16 @@ const About = () => {
               Styled Components
             </p>
             <p>
-              JavaScript
+              JavaScript - ES6
             </p>
             <p>
               React
             </p>
             <p> 
               Git
+            </p>
+            <p> 
+              npm
             </p>
           </div>
         </div>
