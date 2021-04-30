d89a96fd03470bade8533b0674013d7ae971e1a7
Martin <martinroldan889@gmail.com>
2021-04-30 19:34:58 -0300

    Mensaje de alerta al enviar un correo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,32 @@
-import React  from 'react'
-import { onHandleSendEmail } from '../utils/email'
+import React, { useState }  from 'react'
+import emailjs from 'emailjs-com'
 import '../assets/styles/components/Contact.css'
 
 const Contact = () => {
+  const [ value, setValue ] = useState(false)
+  const [ message, setMessage ] = useState('')
+
+  const handleSendEmail = e => {
+    e.preventDefault()
+    
+    emailjs.sendForm('service_mxrold', 'template_1cyjmfz', e.target, 'user_dRUN4n4ny7idTPbrdVRvs')
+      .then((response) => {
+        setValue(true)
+        setMessage('¡Correo enviado! Estaré respondiendo a tu mensaje pronto 😄')
+        setTimeout(() => {
+          e.target.reset()
+        }, 6000)
+      }, (error) => {
+          setValue(true)
+          setMessage('🧟 Hubo un problema y no se envió el correo. Házmelo saber en mis redes sociales')
+    });
+
+    setTimeout(() => {
+      setValue(false)
+      setMessage('')
+    }, 10000)
+  }
+
   return (
     <section className="Contact">
       <div className="Contact__info">
@@ -10,7 +34,7 @@ const Contact = () => {
         <p>Llegaste hasta la parte de contacto 🚀... Espero que te hayan gustado mis proyectos. Si quieres darme alguna recomendación, charlar sobre diversos temas o ponerte en contacto conmigo puedes hacerlo mediante el formulario y también por las redes sociales. ¡Será un gusto conversar contigo!</p>
       </div>
       <div className="Contact__me">
-        <form onSubmit={onHandleSendEmail}>
+        <form action="post" onSubmit={handleSendEmail}>
           <label htmlFor="name">Nombre</label>
           <input type="text" name="name" id="name" placeholder="Nombre" required/>
           <label htmlFor="email">Correo</label>
@@ -18,7 +42,14 @@ const Contact = () => {
           <label htmlFor="message">Mensaje</label>
           <textarea name="message" id="message" placeholder="Mensaje" required/>
           <button type="submit">Enviar</button>
-        </form>  
+          {
+            value &&
+            <div className="Contact__me--alert">
+              <p>{message}</p>
+            </div>
+          }
+        </form>
+        
       </div>
     </section>
   )
