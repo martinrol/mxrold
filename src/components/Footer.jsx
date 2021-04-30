c9c29fa14b65d2bcb83c7f12d3f44468aab27dd1
Martin <martinroldan889@gmail.com>
2021-04-30 20:07:37 -0300

    Update sobre el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,13 @@ import '../assets/styles/components/Footer.css'
 const Footer = () => {
     return (
         <section className="Footer">
-            <a href="#Home" title="Volver al principio">
-                <AiOutlineUp size="28px"/>
-            </a>
+            <div className="Footer__circle">
+                <div className="Footer__circle--link">
+                    <a href="#Home" title="Volver al principio">
+                        <AiOutlineUp size="28px"/>
+                    </a>
+                </div>
+            </div>
         </section>
     )
 }
